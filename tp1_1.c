eaea264deea40764ec1088e2a849f5c1ce1b2871
Claribel <claridiaz02@gmail.com>
2025-04-05 16:17:48 -0300

    resolucion de consigna 3e

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf("Hola Mundo");
+    int a = 10;
+    int *punt = &a;
+    printf("Contenido del puntero: %d ", *punt);
+    printf("\nDireccion almacenada por el puntero: %p" , punt);
+    printf("\nDireccion de memoria la variable: %p", &a);
+    printf("\nDireccion de memoria del puntero: %p", &punt);
+    printf("\nTamaño en memoria de la variable : %lu bytes", sizeof(a));
     return 0;
 }
